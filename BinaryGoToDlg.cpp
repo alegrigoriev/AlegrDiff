3528d9c9998b8ab5b3aa733e41bb994cfc19a96e
Alexander Grigoriev <alegr@earthlink.net>
2006-05-21 08:27:29 -0700

    Changed BinaryGoToDlg.cpp
    FileLength argument added to constructor.
    m_FileLength, m_InitialOffset members added.
    GetNewOffset() function added.
    Validate offset. Support +/- offset from the current position.

--- BinaryGoToDlg.cpp
+++ BinaryGoToDlg.cpp
@@ -4,14 +4,15 @@
 #include "stdafx.h"
 #include "AlegrDiff.h"
 #include "BinaryGoToDlg.h"
-
+#include <limits.h>
 
 // CBinaryGoToDlg dialog
 
-CBinaryGoToDlg::CBinaryGoToDlg(ULONGLONG FileOffset, CWnd* pParent /*=NULL*/)
-	: CDialog(CBinaryGoToDlg::IDD, pParent)
-	, m_OffsetStr(_T(""))
+CBinaryGoToDlg::CBinaryGoToDlg(ULONGLONG FileOffset, ULONGLONG FileLength, CWnd* pParent /*=NULL*/)
+	: BaseClass(IDD, pParent)
 	, m_FileOffset(FileOffset)
+	, m_InitialOffset(FileOffset)
+	, m_FileLength(FileLength)
 {
 }
 
@@ -19,31 +20,95 @@ CBinaryGoToDlg::~CBinaryGoToDlg()
 {
 }
 
+#define INVALID_STRING_RESULT (ULLONG_MAX)
+#define OUT_OF_BOUNDS_RESULT (ULLONG_MAX-1)
+
 void CBinaryGoToDlg::DoDataExchange(CDataExchange* pDX)
 {
-	CDialog::DoDataExchange(pDX);
+	BaseClass::DoDataExchange(pDX);
+	CString OffsetStr;
 	if ( ! pDX->m_bSaveAndValidate)
 	{
-		m_OffsetStr.Format(_T("%I64X"), m_FileOffset);
+		OffsetStr.Format(_T("%I64X"), m_FileOffset);
 	}
 
-	DDX_Text(pDX, IDC_EDIT1, m_OffsetStr);
-	DDV_MaxChars(pDX, m_OffsetStr, 16);
+	DDX_Text(pDX, IDC_EDIT1, OffsetStr);
 
 	if (pDX->m_bSaveAndValidate)
 	{
-		TCHAR * endptr = NULL;
-		m_FileOffset = _tcstoui64(m_OffsetStr, & endptr, 16);
+		m_FileOffset = GetNewOffset(OffsetStr);
+		if (INVALID_STRING_RESULT == m_FileOffset)
+		{
+			CString s;
+			s.Format(IDS_WRONG_BINARY_OFFSET_FORMAT, LPCTSTR(OffsetStr));
+			AfxMessageBox(s);
+			pDX->Fail();
+		}
+		else if (OUT_OF_BOUNDS_RESULT == m_FileOffset)
+		{
+			CString s;
+			s.Format(IDS_OUT_OF_BOUNDS_OFFSET_FORMAT, LPCTSTR(OffsetStr));
+			AfxMessageBox(s);
+			pDX->Fail();
+		}
+	}
+}
+
+ULONGLONG CBinaryGoToDlg::GetNewOffset(LPCTSTR str)
+{
+	TCHAR * endptr = NULL;
+	ULONGLONG num;
+	if (str[0] == _T('+'))
+	{
+		num = _tcstoui64(str + 1, & endptr, 16);
+		if (NULL == endptr
+			|| 0 != *endptr)
+		{
+			return INVALID_STRING_RESULT;
+		}
+		if (num > m_FileLength - m_InitialOffset)
+		{
+			return OUT_OF_BOUNDS_RESULT;
+		}
+		return m_InitialOffset + num;
+	}
+	else if (str[0] == _T('-'))
+	{
+		num = _tcstoui64(str + 1, & endptr, 16);
+		if (NULL == endptr
+			|| 0 != *endptr)
+		{
+			return INVALID_STRING_RESULT;
+		}
+		if (num > m_InitialOffset)
+		{
+			return OUT_OF_BOUNDS_RESULT;
+		}
+		return m_InitialOffset - num;
+	}
+	else
+	{
+		num = _tcstoui64(str, & endptr, 16);
+		if (NULL == endptr
+			|| 0 != *endptr)
+		{
+			return INVALID_STRING_RESULT;
+		}
+		if (num > m_FileLength)
+		{
+			return OUT_OF_BOUNDS_RESULT;
+		}
+		return num;
 	}
 }
 
 BOOL CBinaryGoToDlg::OnInitDialog()
 {
-	CDialog::OnInitDialog();
+	BaseClass::OnInitDialog();
 	return TRUE;  // return TRUE  unless you set the focus to a control
 }
 
-BEGIN_MESSAGE_MAP(CBinaryGoToDlg, CDialog)
+BEGIN_MESSAGE_MAP(CBinaryGoToDlg, BaseClass)
 END_MESSAGE_MAP()
 
 // CBinaryGoToDlg message handlers

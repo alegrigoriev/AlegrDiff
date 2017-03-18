5323f2962534823d85a91897b105c308787bccc3
Alex Grigoriev <alegrigoriev@gmail.com>
2017-03-18 10:48:43 -0700

    Use common.ver to build the version resource

--- resource.h
+++ resource.h
@@ -313,7 +313,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
-#define _APS_NEXT_RESOURCE_VALUE        185
+#define _APS_NEXT_RESOURCE_VALUE        186
 #define _APS_NEXT_COMMAND_VALUE         32911
 #define _APS_NEXT_CONTROL_VALUE         1086
 #define _APS_NEXT_SYMED_VALUE           106
